6e3d6cf3aa237f9cc1976aec5fb443df134f9667
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-07-01 15:33:20 +0330

    provider.tsx setup and used it in layout.tsx

---
+++ src/app/providers.tsx
@@ -0,0 +1,20 @@
+'use client'
+
+import { ReactNode } from 'react'
+import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { store, persistor } from '@/store/store'
+
+type Props = {
+    children: ReactNode
+}
+
+export default function Providers({ children }: Props) {
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                {children}
+            </PersistGate>
+        </Provider>
+    )
+}
