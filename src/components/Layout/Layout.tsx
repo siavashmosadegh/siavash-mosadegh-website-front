72a6c88484557902bec83f0bfdf8ab8deeba32e5
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-07-05 16:38:20 +0330

    implementing Layout.tsx ...

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import React, { ReactNode } from 'react';
+//import Header from '../Main/Header';
+//import ResponsiveHeader from '../Main/ResponsiveHeader';
+//import Footer from './Footer';
+import styles from './Layout.module.scss';
+
+interface LayoutProps {
+    children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+        <div className={styles.layoutWrapper}>
+            <header className={styles.headerWrapper}>
+                {/* <Header /> */}
+            </header>
+
+            <header className={styles.responsiveHeaderWrapper}>
+                {/* <ResponsiveHeader /> */}
+            </header>
+
+            <main className={styles.childrenWrapper}>
+                {children}
+            </main>
+
+            <footer>
+                {/* <Footer /> */}
+            </footer>
+        </div>
+    );
+};
+
+export default Layout;
