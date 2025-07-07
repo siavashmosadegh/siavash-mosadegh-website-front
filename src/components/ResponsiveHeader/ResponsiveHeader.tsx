25fa8c3359a00e6691e96bc0a7f1db6dca1899c8
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-07-07 15:12:07 +0330

    used Drawer in ResponsiveHeader

--- src/components/ResponsiveHeader/ResponsiveHeader.tsx
+++ src/components/ResponsiveHeader/ResponsiveHeader.tsx
@@ -1,10 +1,36 @@
 import { Menu } from 'lucide-react';
 import styles from './ResponsiveHeader.module.scss';
+import Drawer from '../Drawer/Drawer';
+import { useState } from 'react';
+import Link from 'next/link';
 
 const ResponsiveHeader: React.FC = () => {
+
+    const [drawerIsOpen, setDrawerIsOpen] = useState <boolean> (false);
+
+    const handleOpenDrawer = (): void => setDrawerIsOpen(true);
+    const handleCloseDrawer = (): void => setDrawerIsOpen(false);
+
     return (
         <div className={styles.wrapper}>
-            <Menu color="white" className={styles.icon}/>
+            <Menu
+                color="white"
+                className={styles.icon}
+                onClick={() => handleOpenDrawer()}
+            />
+
+            <Drawer
+                isOpen={drawerIsOpen}
+                position="left"
+                onClose={handleCloseDrawer}
+            >
+                <ul className={`${styles.navLinks} ${drawerIsOpen ? styles.show : ''}`}>
+                    <li><Link href="/">Home</Link></li>
+                    <li><Link href="/about">About</Link></li>
+                    <li><Link href="/services">Services</Link></li>
+                    <li><Link href="/contact">Contact</Link></li>
+                </ul>
+            </Drawer>
         </div>
     );
 }
