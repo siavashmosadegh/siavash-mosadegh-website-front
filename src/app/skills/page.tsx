9d9d26b764f7a38157e1cf1a8e482960d7e0f7a8
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-07-09 14:10:49 +0330

    changed grid system and added some icons

--- src/app/skills/page.tsx
+++ src/app/skills/page.tsx
@@ -13,87 +13,186 @@ import ExpressJsIcon from "../../../public/icons/ExpressJsIcon";
 import NginxIcon from "../../../public/icons/NginxIcon";
 import DockerIcon from "../../../public/icons/DockerIcon";
 import MongodbIcon from "../../../public/icons/MongodbIcon";
+import GitIcon from "../../../public/icons/GitIcon";
 
 const Skills: React.FC = () => {
     return (
-        <div className={styles.column}>
-            <div className={styles.row}>
-                <div className={styles.javascriptIcon}>
-                    <JavascriptIcon />
+        <div className={styles.row}>
+            <div className={styles.column}>
 
-                    <p>Javascript</p>
+                <div className={styles.square}>
+                    <p>Language</p>
                 </div>
 
-                <div className={styles.typescriptIcon}>
-                    <TypescriptIcon />
-
-                    <p>Typescript</p>
+                <div className={styles.square}>
+                    <p>Frameworks</p>
                 </div>
 
-                <div className={styles.nodejsIcon}>
-                    <NodejsIcon />
-
-                    <p>Node.js</p>
+                <div className={styles.square}>
+                    <p>Database</p>                
                 </div>
+
+                <div className={styles.square}>
+                    <p>Tools</p>
+                </div>
+
             </div>
 
-            <div className={styles.row}>
-                <div className={styles.reactIcon}>
-                    <ReactIcon />
+            <div className={styles.column}>
 
-                    <p>React.js</p>
+                <div className={styles.square}>
+                    <div className={styles.javascriptIcon}>
+                        <JavascriptIcon />
+
+                        <p>Javascript</p>
+                    </div>
                 </div>
 
-                <div className={styles.nextjsIcon}>
-                    <NextjsIcon />
-                    
-                    <p>Next.js</p>
+                <div className={styles.square}>
+                    <div className={styles.reactIcon}>
+                        <ReactIcon />
+
+                        <p>React.js</p>
+                    </div>                    
                 </div>
 
-                <div className={styles.expressJsIcon}>
-                    <ExpressJsIcon />
+                <div className={styles.square}>
+                    <div className={styles.postgresqlIcon}>
+                        <PostgresqlIcon />
 
-                    <p>Express.js</p>
+                        <p>PostgreSQL</p>
+                    </div>
                 </div>
+
+                <div className={styles.square}>
+                    <div className={styles.nginxIcon}>
+                        <NginxIcon />
+
+                        <p>Nginx</p>
+                    </div>                    
+                </div>
+
             </div>
 
-            <div className={styles.row}>
-                <div className={styles.mongodbIcon}>
-                    <MongodbIcon />
+            <div className={styles.column}>
 
-                    <p>MongoDB</p>
+                <div className={styles.square}>
+                    <div className={styles.typescriptIcon}>
+                        <TypescriptIcon />
+
+                        <p>Typescript</p>
+                    </div>
                 </div>
 
-                <div className={styles.postgresqlIcon}>
-                    <PostgresqlIcon />
-
-                    <p>PostgreSQL</p>
+                <div className={styles.square}>
+                    <div className={styles.nextjsIcon}>
+                        <NextjsIcon />
+                        
+                        <p>Next.js</p>
+                    </div>                    
                 </div>
 
-                <div className={styles.microsoftSqlServerIcon}>
-                    <MicrosoftSqlServerIcon />
+                <div className={styles.square}>
+                    <div className={styles.microsoftSqlServerIcon}>
+                        <MicrosoftSqlServerIcon />
 
-                    <p>Microsoft SQL Server</p>
+                        <p>Microsoft SQL Server</p>
+                    </div>                 
                 </div>
+
+                <div className={styles.square}>
+                    <div className={styles.dockerIcon}>
+                        <DockerIcon />
+
+                        <p>Docker</p>
+                    </div>
+                </div>
+
             </div>
 
-            <div className={styles.row}>
+            <div className={styles.column}>
 
-                <div className={styles.nginxIcon}>
-                    <NginxIcon />
+                <div className={styles.square}>
+                    <div className={styles.nodejsIcon}>
+                        <NodejsIcon />
 
-                    <p>Nginx</p>
+                        <p>Node.js</p>
+                    </div>
                 </div>
 
-                <div className={styles.dockerIcon}>
-                    <DockerIcon />
+                <div className={styles.square}>
+                    <div className={styles.expressJsIcon}>
+                        <ExpressJsIcon />
 
-                    <p>Docker</p>
+                        <p>Express.js</p>
+                    </div>
+                </div>
+
+                <div className={styles.square}>
+                    <div className={styles.mongodbIcon}>
+                        <MongodbIcon />
+
+                        <p>MongoDB</p>
+                    </div>
+                </div>
+
+                <div className={styles.square}>
+                    <div className={styles.gitIcon}>
+                        <GitIcon />
+
+                        <p>Git</p>
+                    </div>
                 </div>
             </div>
-
         </div>
     );
 }
 
 export default Skills;
+        // <div className={styles.column}>
+        //     <div className={styles.row}>
+
+
+        //         <div className={styles.nodejsIcon}>
+        //             <NodejsIcon />
+
+        //             <p>Node.js</p>
+        //         </div>
+        //     </div>
+
+        //     <div className={styles.row}>
+
+
+        //         <div className={styles.expressJsIcon}>
+        //             <ExpressJsIcon />
+
+        //             <p>Express.js</p>
+        //         </div>
+        //     </div>
+
+        //     <div className={styles.row}>
+        //         <div className={styles.mongodbIcon}>
+        //             <MongodbIcon />
+
+        //             <p>MongoDB</p>
+        //         </div>
+
+
+        //     </div>
+
+        //     <div className={styles.row}>
+
+        //         <div className={styles.nginxIcon}>
+        //             <NginxIcon />
+
+        //             <p>Nginx</p>
+        //         </div>
+
+        //         <div className={styles.dockerIcon}>
+        //             <DockerIcon />
+
+        //             <p>Docker</p>
+        //         </div>
+        //     </div>
+
+        // </div>
