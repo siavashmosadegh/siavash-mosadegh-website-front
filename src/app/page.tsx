3da84757d0254f531f3f4c5388646caadf9bf829
siavashmosadegh <s.mosadegh@rocketmail.com>
2025-07-12 12:27:18 +0330

    started working on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
 
-export default function Home() {
+import { motion } from 'framer-motion';
+import styles from './page.module.scss';
+
+export default function LandingPage() {
   return (
-    <div className={styles.page}>
-      Siavash Mosadegh | Software Engineer
+    <div className={styles.container}>
+      <motion.h1
+        className={styles.name}
+        initial={{ x: '-100vw', opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ type: 'spring', stiffness: 80, delay: 1 }}
+      >
+        Siavash Mosadegh
+      </motion.h1>
+
+      <motion.h2
+        className={styles.title}
+        initial={{ x: '100vw', opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        transition={{ type: 'spring', stiffness: 80, delay: 1.5 }}
+      >
+        Software Engineer
+      </motion.h2>
     </div>
   );
 }
